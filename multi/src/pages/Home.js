06f3711a63ecc31aad1504f0eb79f89f41d63d38
Brian Hayes <briandhayes01@gmail.com>
2022-10-29 20:05:57 +0000

    Here is the app

---
+++ multi/src/pages/Home.js
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+      <>
+      
+      <h1>Welcome to our bubble wrap sim!</h1>
+      <h2>Click on 'Bubbles' to get going! </h2>
+      
+      
+      </>
+    )
+};
+
+export default Home;
